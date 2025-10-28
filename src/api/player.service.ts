edbed80c5935ad03c38c69364125c7f07cea6de1
Alexandre Dissaux <alexandre.dissaux.ext@orange.com>
2025-10-28 14:00:40 +0100

    feat(): adding cache to palyer request

--- src/api/player.service.ts
+++ src/api/player.service.ts
@@ -1,17 +1,36 @@
+// Cache to store player data by player ID
+const playerCache = new Map<string, any>();
+
+
+
 export async function getPlayer(playerId: string): Promise<any> {
-    const response = await fetch(`https://aoe4world.com/api/v0/players/${playerId}`);
-    const playerdata = await response.json();
-    if (!playerdata.modes.rm_solo) {
-        playerdata.modes.rm_solo = {
+    // Check if player data is already in cache
+    if (playerCache.has(playerId)) {
+        return playerCache.get(playerId)
+    }
+
+    // Fetch player data if not in cache
+    const playerData = (await fetch(`https://aoe4world.com/api/v0/players/${playerId}`)).json();
+    const player = transformPlayerData(playerData);
+    
+    // Store in cache before returning
+    playerCache.set(playerId, player)
+    
+    return player;
+} 
+
+
+const transformPlayerData = (playerData: any) => {
+    if (!playerData.modes.rm_solo) {
+        playerData.modes.rm_solo = {
             win_rate: 0,
             wins_count: 0,
             losses_count: 0,
         }
     }
-    playerdata.modes.rm_solo.win_rate = Number(playerdata.modes.rm_solo.win_rate).toFixed(1);
-    return playerdata;
-} 
-
+    playerData.modes.rm_solo.win_rate = Number(playerData.modes.rm_solo.win_rate).toFixed(1);
+    return playerData;
+}
 
 export function getAllPlayer(players: any[], teamName: string, acronyme: string): Promise<any[]> {
     const promises = players.map(async(player) => {
