c6426e39347f8c2a7bd0cfad398423cca9a8929a
Alexandre Dissaux <alexandre.dissaux.ext@orange.com>
2025-11-07 22:17:31 +0100

    official teams

--- src/db/data.ts
+++ src/db/data.ts
@@ -5,7 +5,7 @@ export type TeamsNameAndId = {
     players : Array<{id: string, isCap? : boolean}>
 }
 
-export const sinceDate = '2025/09/01'
+export const sinceDate = '2025/11/08'
 
 
 export const teamsNameAndId: TeamsNameAndId[] = [
@@ -14,11 +14,11 @@ export const teamsNameAndId: TeamsNameAndId[] = [
         points: 0,
         acronyme: "Eq1",
         players : [
-            {id: "20391264", isCap : true}, // LeBigl
-            {id: "8953699", isCap : false}, // Ours
+            {id: "8953699", isCap : true}, // Ours
+            {id: "15624724", isCap : false}, // Stay_Frosty
+            {id: "7319091", isCap : false}, // Slender
+            {id: "21796931", isCap : false}, // Totox
             {id: "17594012", isCap : false}, // Rod
-            {id: "1432106", isCap : false}, // Cjee
-            {id: "10605002", isCap : false}, // Val
         ]
     },
     {    
@@ -26,11 +26,11 @@ export const teamsNameAndId: TeamsNameAndId[] = [
         points: 0,
         acronyme: "Eq2",
         players : [
-            {id: "20704115", isCap : true}, // Manishax_Calni
-            {id: "9254518", isCap : false}, // Caius
-            {id: "1365135", isCap : false}, // La_CAF
-            {id: "5556271", isCap : false}, // Joepescie
-            {id: "22658829", isCap : false}, // Eleva
+            {id: "22658829", isCap : true}, // Eleva
+            {id: "15289018", isCap : false}, // Tarlanors
+            {id: "14400153", isCap : false}, // The_Vegas
+            {id: "1421448", isCap : false}, // Xelforce
+            {id: "15012300", isCap : false}, // Oulala
         ]
     },
     {    
@@ -38,11 +38,11 @@ export const teamsNameAndId: TeamsNameAndId[] = [
         points: 0,
         acronyme: "Eq3",
         players : [
-            {id: "12029658", isCap : true}, // Alyce
-            {id: "4314006", isCap : false}, // Raptor
-            {id: "6732245", isCap : false}, // Xelkara
+            {id: "5556271", isCap : true}, // Joepescie
+            {id: "6550325", isCap : false}, // Bob
             {id: "790039", isCap : false}, // Marin_Deaudouce
-            {id: "1421448", isCap : false}, // Xelforce
+            {id: "8989366", isCap : false}, // Mascro
+            {id: "20704115", isCap : false}, // Manishax_Calni
         ]
     },
     {    
@@ -50,11 +50,11 @@ export const teamsNameAndId: TeamsNameAndId[] = [
         points: 0,
         acronyme: "Eq4",
         players : [
-            {id: "23866592", isCap : true}, // Druss
-            {id: "21796931", isCap : false}, // Totox
-            {id: "12170036", isCap : false}, // Vox
-            {id: "8989366", isCap : false}, // Mascro
-            {id: "15012300", isCap : false}, // Oulala
+            {id: "1712108", isCap : true}, // Trakor
+            {id: "23800459", isCap : false}, // nicolas
+            {id: "23160801", isCap : false}, // Firefooxs
+            {id: "1947541", isCap : false}, // Rellik
+            {id: "9254518", isCap : false}, // Caius
         ]
     },
     {    
@@ -62,11 +62,11 @@ export const teamsNameAndId: TeamsNameAndId[] = [
         points: 0,
         acronyme: "Eq5",
         players : [
-            {id: "2866310", isCap : true}, // Monster
-            {id: "7319091", isCap : false}, // Slender
-            {id: "8250217", isCap : false}, // Bulky
-            {id: "1947541", isCap : false}, // Rellik
-            {id: "14400153", isCap : false}, // The_Vegas
+            {id: "9754661", isCap : true}, // The_Tost
+            {id: "2866310", isCap : false}, // Monster
+            {id: "358437", isCap : false}, // Epiviosi
+            {id: "12029658", isCap : false}, // Alyce
+            {id: "11667821", isCap : false}, // Force
         ]
     },
     {    
@@ -74,11 +74,12 @@ export const teamsNameAndId: TeamsNameAndId[] = [
         points: 0,
         acronyme: "Eq6",
         players : [
-            {id: "358437", isCap : true}, // Epiviosi
-            {id: "9754661", isCap : false}, // The_Tost
-            {id: "8275065", isCap : false}, // Bucéphale
-            {id: "6550325", isCap : false}, // Bob
-            {id: "22598716", isCap : false}, // Vegeta
+            {id: "22598716", isCap : true}, // Vegeta
+            {id: "23866592", isCap : false}, // Druss
+            {id: "1432106", isCap : false}, // Cjee
+            {id: "10605002", isCap : false}, // Val
+            {id: "6732245", isCap : false}, // Xelkara
+
         ]
     },
     {    
@@ -86,11 +87,11 @@ export const teamsNameAndId: TeamsNameAndId[] = [
         points: 0,
         acronyme: "Eq7",
         players : [
-            {id: "23160801", isCap : true}, // Firefooxs
-            {id: "15624724", isCap : false}, // Stay_Frosty
-            {id: "15289018", isCap : false}, // Tarlanors
-            {id: "1712108", isCap : false}, // Trakor
-            {id: "11575115", isCap : false}, // Coco
+            {id: "19206930", isCap : true}, // Cosmos
+            {id: "12170036", isCap : false}, // Vox
+            {id: "8250217", isCap : false}, // Bulky
+            {id: "1365135", isCap : false}, // La_CAF
+            {id: "20868210", isCap : false}, // ElectroShock
         ]
     },
     {    
@@ -98,9 +99,14 @@ export const teamsNameAndId: TeamsNameAndId[] = [
         points: 0,
         acronyme: "Eq8",
         players : [
-            {id: "19206930", isCap : true}, // Cosmos
-            {id: "18963110", isCap : false}, // Rollos
-            {id: "23800459", isCap : false}, // nicolas
+            {id: "18963110", isCap : true}, // Rollos
+            {id: "11575115", isCap : false}, // Coco
+            {id: "8275065", isCap : false}, // Bucéphale
+            {id: "23387649", isCap : false}, // Niwaj
+            {id: "4314006", isCap : false}, // Raptor
         ]
     }
 ]
+
+
+// {id: "20391264", isCap : false}, // LeBigl
