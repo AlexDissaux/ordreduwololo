cba1ca361c756efdfdb4399b4f4f0d5483b47f1f
Alexandre Dissaux <alexandre.dissaux.ext@orange.com>
2026-01-20 16:46:45 +0100

    chore(): adding hmr hot reloading on nest

---
+++ apps/backend/webpack-hmr.config.js
@@ -0,0 +1,44 @@
+const webpack = require('webpack');
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+const { RunScriptWebpackPlugin } = require('run-script-webpack-plugin');
+
+module.exports = {
+  entry: ['webpack/hot/poll?100', './src/main.ts'],
+  target: 'node',
+  externals: [
+    nodeExternals({
+      allowlist: ['webpack/hot/poll?100'],
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: {
+          loader: 'ts-loader',
+          options: {
+            configFile: 'tsconfig.app.json',
+            transpileOnly: true,
+          },
+        },
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  mode: 'development',
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.WatchIgnorePlugin({
+      paths: [/\.js$/, /\.d\.ts$/],
+    }),
+    new RunScriptWebpackPlugin({ name: 'main.js', autoRestart: false }),
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'main.js',
+  },
+};
