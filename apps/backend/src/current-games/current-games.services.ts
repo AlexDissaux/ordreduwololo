ead5c6c2fd4ed0b954caca68b34f550940732160
AlexDissaux <dissauxalex@gmail.com>
2026-04-17 16:46:10 +0200

    correct current games

--- apps/backend/src/current-games/current-games.services.ts
+++ apps/backend/src/current-games/current-games.services.ts
@@ -4,11 +4,14 @@ import { PlayerService } from "../player";
 import { fetchCurrentGames } from "./current-games.api";
 import { CurrentGameDto } from "./dto/current-games.dto";
 import { toCurrentGameDto } from "./current-games.mapper";
+import { PlayerRepository } from "src/player/player.repository";
 
 
 @Injectable()
 export class CurrentGamesService {
-    constructor(private readonly playerService: PlayerService) {}
+    constructor(private readonly playerService: PlayerService,
+        private readonly playerRepository: PlayerRepository
+    ) {}
 
     private readonly gamesSubject = new BehaviorSubject<CurrentGameDto[]>([]);
 
@@ -24,7 +27,7 @@ export class CurrentGamesService {
     }
 
     public async setCurrentGamesFromActivePlayers(): Promise<void> {
-        const profileIds = await this.playerService.findAllProfileIdsFromActivePlayers();
+        const profileIds = await this.playerRepository.findAllProfileIdsFromActivePlayers();
 
         const games = (await fetchCurrentGames(profileIds))
             .map(({ game, profileId }) => toCurrentGameDto(game, profileId));
